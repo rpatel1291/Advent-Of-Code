696499571c2c11130edd564a57da5ba96415628d
rpatel1291 <rpatel1291@gmail.com>
2021-02-20 23:23:54 -0500

    Day 3 part 2
    - refactor code for part 1
    - added part 2

--- Day3/src/com/rpatel1291/Day3.java
+++ Day3/src/com/rpatel1291/Day3.java
@@ -19,11 +19,46 @@ public class Day3 {
     public static void main(String[] args) {
 
         data = importData(INPUT_FILE);
-        int right = 3;
-        int down = 1;
-        int col = 3;
-//        System.out.println(data.size());
-//        System.out.println(data.get(0).size());
+
+        int resultForPart1 = numberOfTreeEncountered(data,3,1);
+
+        System.out.println("Result for part 1: " + resultForPart1);
+
+        int[] rightSlopes = new int[]{1,3,5,7,1};
+        int[] downSlopes = new int[]{1,1,1,1,2};
+        long resultForPart2 = 1L;
+
+        for(int i = 0 ; i < rightSlopes.length; i++){
+            resultForPart2 *= numberOfTreeEncounteredPart2(importData(INPUT_FILE),rightSlopes[i], downSlopes[i]);
+        }
+
+        System.out.println("Result for part 2: " + resultForPart2);
+
+    }
+
+
+    private static ArrayList<String[]> importData(final String file){
+        ArrayList<String[]> data = null;
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            data = new ArrayList<>();
+            while((line = bufferedReader.readLine())!=null){
+               data.add(line.split(""));
+            }
+            return data;
+        } catch (FileNotFoundException e) {
+            System.out.println(String.format("Error while getting file: {}. Reason: {}",file, e.getMessage()));
+        } catch (IOException e) {
+            System.out.println(String.format("Error while reading file: {}. Reason: {}",file, e.getMessage()));
+        }
+        return data;
+    }
+
+    private static int numberOfTreeEncountered(ArrayList<String[]> data, int right, int down){
+
+        int col = right;
+
         for(int row = down; row < data.size(); row += down){
             String[] line = data.get(row);
             if(line[col].equals(OPEN_SQUARE)) {
@@ -47,30 +82,44 @@ public class Day3 {
         for(String[] i : data){
             for(String j : i){
                 if(j.equals(CHECKED_TREE)) countTree++;
-                System.out.print(j);
+//                System.out.print(j);
             }
-            System.out.println();
+//            System.out.println();
         }
 
-        System.out.println("Result for part 1: " + countTree);
+        return countTree;
     }
 
+    private static int numberOfTreeEncounteredPart2(ArrayList<String[]> data, int right, int down){
 
-    private static ArrayList<String[]> importData(final String file){
-        ArrayList<String[]> data = null;
-        try{
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String line;
-            data = new ArrayList<>();
-            while((line = bufferedReader.readLine())!=null){
-               data.add(line.split(""));
+        int col = 0;
+
+        for(int row = 0; row < data.size(); row += down){
+            String[] line = data.get(row);
+            if(line[col].equals(OPEN_SQUARE)) {
+                line[col] = CHECKED_OPEN;
             }
-            return data;
-        } catch (FileNotFoundException e) {
-            System.out.println(String.format("Error while getting file: {}. Reason: {}",file, e.getMessage()));
-        } catch (IOException e) {
-            System.out.println(String.format("Error while reading file: {}. Reason: {}",file, e.getMessage()));
+            else{
+                if(line[col].equals(TREE)){
+                    line[col] = CHECKED_TREE;
+                }
+            }
+            if (col > 0) {
+                col = (col + right) % line.length;
+            }
+            else{
+                col += right;
+            }
         }
-        return data;
+
+        int countTree = 0;
+
+        for(String[] i : data){
+            for(String j : i){
+                if(j.equals(CHECKED_TREE)) countTree++;
+            }
+        }
+
+        return countTree;
     }
 }
